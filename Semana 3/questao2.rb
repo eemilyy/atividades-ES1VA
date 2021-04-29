562a812cabb0e8b050808a8c24171d28b05c1db2
Emily Santos <emily.mimy12@gmail.com>
2021-04-29 20:01:42 -0300

    q2 respondida

--- Semana 3/questao2.rb
+++ Semana 3/questao2.rb
@@ -1,5 +1,14 @@
-linhasN = gets.chomp.to_i
+n = File.open("entradaQ2")
+arr = Array.new
 
-for i in 1..linhasN
-    puts i
+File.open("entradaQ2").each do |m|
+    arr[m.split(" ")[0].to_i - 1] = m.split(" ")[1]
+    #arr[m[0]] = m[1]
+    #p arr
 end
+#n File.open("entradaQ2")
+arr.each do |i|
+    print i
+end
+#p File.readlines("entradaQ2").slice
+
