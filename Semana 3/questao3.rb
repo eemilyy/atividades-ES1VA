aa47eedb50d634b96ae77f87ea9d6fe5b42dfcba
Emily Santos <emily.mimy12@gmail.com>
2021-05-01 22:56:53 -0300

    q3 finalizada

--- Semana 3/questao3.rb
+++ Semana 3/questao3.rb
@@ -1,44 +1,24 @@
 arr = []
-contador = 0
-tigres = 0
 macacos = 0
 pesotigres = 0
 cobras_venezuela = 0
-qtd = 0
+qtd_tigres = 0
 
 File.open("entradaQ3").each do |animal|           
-    #animal.upcase == "PARAR\n" ? (arr << animal) : break
-
-    # if(animal.upcase != "CONTINUAR\n")
-        # arr << animal
-    # end   
-
-     if animal.chomp.upcase != "CONTINUAR" and animal.chomp.upcase != "PARAR" #a menos que
-          arr << animal.chomp.upcase
-        # p "entrou"
-     
-
+    if animal.chomp.upcase != "CONTINUAR" and animal.chomp.upcase != "PARAR"
+        arr << animal.chomp.upcase        
+        
     else
-        #p "2"
-    
-    # unless animal.chomp.upcase == "CONTINUAR" #a menos que
-        # arr << animal.chomp.upcase
-        # next
-    # end
-
-    macacos += 1 if arr[0] == "MACACO"
-    pesotigres += arr[1].to_f and qtd += 1 if arr[0] == "TIGRE"
-    cobras_venezuela += 1 if (arr[0] == "COBRA") and (arr[2] == "VENEZUELA")
-    
-    arr.clear
-
+        macacos += 1 if arr[0] == "MACACO"
+        pesotigres += arr[1].to_f and qtd_tigres += 1 if arr[0] == "TIGRE"
+        cobras_venezuela += 1 if (arr[0] == "COBRA") and (arr[2] == "VENEZUELA")        
+        arr.clear        
     end
+
+    break if animal.chomp.upcase == "PARAR"
 end
 
-#p qtd
-#print arr
-
 puts macacos
-qtd == 0 ? (puts '0.00') : (puts pesotigres/qtd) 
+qtd_tigres == 0 ? (puts '0.00') : (puts '%.2f' % (pesotigres/qtd_tigres)) 
 puts cobras_venezuela
 
