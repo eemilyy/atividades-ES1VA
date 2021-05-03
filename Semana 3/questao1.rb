36c44c852f8ea772cf861a8ed91b7cead80099d5
Emily Santos <emily.mimy12@gmail.com>
2021-05-03 13:33:46 -0300

    Atividade final

--- Semana 3/questao1.rb
+++ Semana 3/questao1.rb
@@ -10,19 +10,19 @@ class Time
 
         if diferenca <= 0.5
             if horas == 0
-                "About midnight"
+                "About Midnight"
             else  
                 "About " + horas.to_s + ":00 " + ampm
             end
-        elsif diferenca > 0.5 and diferenca < 1.5
+        elsif diferenca > 0.5 and diferenca <= 1.5
             horas == 0 ? "About 12:15 " + ampm : "About " + horas.to_s + ":15 " + ampm
-        elsif diferenca > 1.5 and diferenca < 2.5
+        elsif diferenca > 1.5 and diferenca <= 2.5
             horas == 0 ? "About 12:30 " + ampm : "About " + horas.to_s + ":30 " + ampm
         elsif diferenca > 2.5 and diferenca <= 3.5
             horas == 0 ? "About 12:45 " + ampm : "About " + horas.to_s + ":45 " + ampm
         else
             if horas == 23
-                "About midnight"
+                "About Midnight"
             else
                 "About " + (horas+1).to_s + " " +  ampm
             end
